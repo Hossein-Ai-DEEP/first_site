408b8b4a09dc9e4a036040486c214d64fb5c33c0
hossein hadipour <175479768+Hossein-Ai-DEEP@users.noreply.github.com>
2026-05-11 21:52:25 +0330

    use html templates

--- website/views.py
+++ website/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 
 def home_view(request):
-    return HttpResponse('<h1>hello my friend you in the home page</h1>')
+    return render(request, 'index.html')
 
 
 def about_view(request):
-    return HttpResponse('<h1>hello my friend you in the about page </h1>')
+    return render(request, 'about.html')
 
 
 def contact_view(request):
-    return HttpResponse('<h1>hello my friend you in the contact page</h1>')
+    return render(request, 'contact.html')
 
 # Create your views here.
